3252538a9b62ec7c4a05d81a0e3527282e7ecee7
ab1228 <alexb8734@gmail.com>
2019-08-27 01:09:56 -0500

    fixed loop for letter left to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,12 +21,49 @@ var wordsArray = [
     "rain",
 ]
 
-function renderWord() {
-    var randomWord = wordsArray
-    randomWord = randomWord[Math.floor(Math.random() * randomWord.length)];
-    console.log(randomWord);
-    if (randomWord <= wordsArray.length - 1) {
-        document.querySelector("#word").innerHTML = wordsArray[0];
+
+
+
+var randomWord = wordsArray
+randomWord = randomWord[Math.floor(Math.random() * randomWord.length)];
+
+var lettersLeft = randomWord.length
+var wins = 0;
+
+window.onload = function renderWord() {
+
+
+
+    randomLetters = Array.from(randomWord);
+    // console.log(randomLetters);
+    for (i = 0; i < randomWord.length; i++) {
+        randomLetters[i] = "_"
+        // console.log(randomWord[0]);
+        document.querySelector("#word").innerHTML = randomLetters;
+
+    }
+
+
+
+
+}
+
+document.onkeyup = function startGame() {
+    var userGuess = event.key;
+    console.log(userGuess);
+
+    if (randomWord.includes(userGuess)) {
+        for (k = 0; k < randomWord.length; k++) {
+            if (randomWord[k] === userGuess) {
+                randomLetters[k] === userGuess;
+            }
+        }
+        lettersLeft--;
+        document.querySelector("#guesses-left").innerHTML = lettersLeft;
     }
 }
-renderWord()
+
+
+
+
+
