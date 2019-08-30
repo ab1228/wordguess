3815aed07d2b999808d57a20fdf9bd9414b6b0c5
ab1228 <alexb8734@gmail.com>
2019-08-30 12:19:49 -0500

    fixed losing function to show new word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+// ARRAY OF WORDS
 var wordsArray = [
     "appear",
     "disgust",
@@ -23,7 +25,7 @@ var wordsArray = [
 
 
 
-
+// TURN ARAY INTO VAR OF RANDOM WORDS
 var randomWord = wordsArray
 randomWord = randomWord[Math.floor(Math.random() * randomWord.length)];
 
@@ -35,29 +37,31 @@ window.onload = function renderWord() {
     var wins = 0;
     wrongGuesses = [];
     arrayrandomLetters = [];
-
+    document.getElementById('guesses-left').innerHTML = lettersLeft;
+    document.getElementById('user-wins').innerHTML = wins;
+    document.getElementById("letters-guessed").innerHTML = wrongGuesses;
+    // TRUNING WORDS INTO _
     arrayrandomLetters = Array.from(randomWord);
     // console.log(arrayrandomLetters);
     for (var i = 0; i < randomWord.length; i++) {
-        arrayrandomLetters[i] = "_"
-        var stashComma = arrayrandomLetters.join(" ")
+        arrayrandomLetters[i] = "_";
         // console.log(randomWord[0]);
         document.querySelector("#word").innerHTML = arrayrandomLetters.join(" ");
 
     }
 
 
-    document.getElementById('guesses-left').innerHTML = lettersLeft;
-    document.getElementById('user-wins').innerHTML = wins;
-    document.getElementById("letters-guessed").innerHTML = wrongGuesses;
+    // document.getElementById('guesses-left').innerHTML = lettersLeft;
+    // document.getElementById('user-wins').innerHTML = wins;
+    // document.getElementById("letters-guessed").innerHTML = wrongGuesses;
 
 
 
-
+    // STARTING THE GAME
     document.onkeyup = function startGame() {
         var userGuess = event.key;
         console.log(userGuess);
-
+        // CAPTURING USER GUESS
         if (randomWord.includes(userGuess)) {
             for (var k = 0; k < randomWord.length; k++) {
                 if (randomWord[k] === userGuess) {
@@ -72,12 +76,14 @@ window.onload = function renderWord() {
             document.querySelector("#word").innerHTML = arrayrandomLetters.join(" ");
             document.getElementById('guesses-left').innerHTML = lettersLeft;
         }
+        // WINS
         if (lettersLeft <= 0 && arrayrandomLetters.includes("_") != true) {
-            wins++
+            wins++;
             document.getElementById("user-wins").innerHTML = wins;
 
         }
         else {
+            // SHOWING WRONG GUESSED WORDS
             var userGuess = event.key;
             for (var f = 0; f < randomWord.length; f++) {
                 if (randomWord.indexOf(userGuess) === -1) {
@@ -86,15 +92,27 @@ window.onload = function renderWord() {
                 }
             }
             document.getElementById("letters-guessed").innerHTML = wrongGuesses.join(" ");
-            lettersLeft--
-            document.getElementById('guesses-left').innerHTML = lettersLeft
+            lettersLeft--;
+            document.getElementById('guesses-left').innerHTML = lettersLeft;
 
 
 
         }
         if (lettersLeft === 0) {
             document.getElementById("game-over").innerHTML = "Your letters did not equal the word. Press any letters tp start again";
-            startGame();
+            document.onkeyup = function (event) {
+                // if (event.keyCode > 64 && event.keyCode < 91) {
+                //     window.location.reload();
+                // }
+                // var userGuess = event.key;
+                window.location.reload();
+
+            }
+
+
+
+
+
         }
 
 
